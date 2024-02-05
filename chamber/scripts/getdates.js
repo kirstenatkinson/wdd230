e9d3ba06a140b08de209110b01d23f15779857ac
kirstenatkinson <atk20002@byui.edu>
2024-02-05 16:44:14 -0700

    chamber site

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,14 @@
+
+
+//a function to display the current copyright year and information in my HTML footer
+function displayYear() {
+    document.getElementById("copyrightName").innerHTML = `&copy ${new Date().getFullYear()} Kirsten Atkinson USA`;
+}
+
+//a function to display when the document was last modified
+function displayLastModified() {
+    document.getElementById("lastModified").innerHTML = `Last Modified: ${new Date(document.lastModified)}`
+}
+
+displayYear();
+displayLastModified();
