a60a994cd666466d94d43d753202c711fb32521a
kirstenatkinson <atk20002@byui.edu>
2024-02-07 14:06:27 -0700

    fixing bom bugs

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,23 +1,21 @@
-const input = document.querySelector('favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('list');
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#submit');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
     if (input.value != '') {
+        console.log('Hello WOrld');
         const li = document.createElement('li');
-        const deleteButton = document.createELement('button');
+        const deleteButton = document.createElement('button');
         li.textContent = input.value;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
-        list.append(li);
         deleteButton.addEventListener('click', () => {
             list.removeChild(li);
             input.focus();
         });
+        list.append(li);
         input.focus();
         input.value = '';
     }
-})
-button.addEventListener('click', () => {
-
-})
+});
