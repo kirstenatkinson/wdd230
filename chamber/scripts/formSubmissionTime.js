6c34a217a32fb01de7a378a0a22faf752b77edd0
kirstenatkinson <atk20002@byui.edu>
2024-03-05 17:14:05 -0700

    fixing errors

---
+++ chamber/scripts/formSubmissionTime.js
@@ -0,0 +1,10 @@
+
+let button = document.getElementById("submit-join-request");
+
+button.addEventListener("click", submitTime);
+
+function submitTime() {
+    let currentTime = Date.now();
+    document.getElementById("submission-time").value = currentTime;
+    console.log(currentTime);
+}
