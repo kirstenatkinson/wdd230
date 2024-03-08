bb7b84c096d34239675366c5d56402267a5da13e
kirstenatkinson <atk20002@byui.edu>
2024-03-08 13:09:49 -0700

    fixing link JSON javascript

--- scripts/links.js
+++ scripts/links.js
@@ -5,21 +5,26 @@ const list = document.querySelector('#learning-activities');
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    //console.log(data);
-    displayLinks(data);
+    console.log(data);
+    displayLinks(data.lessons);
 }
 
-function displayLinks(weeks) {
+const displayLinks = (weeks) => {
     weeks.forEach((week) => {
         let listWeek = document.createElement('li');
         listWeek.textContent = `Week ${week.lesson}: `;
 
-        week.links.forEach((link) => {
+        week.links.forEach((link, index) => {
             let activityLink = document.createElement('a');
             activityLink.setAttribute('href', `${link.url}`);
             activityLink.setAttribute('target', '_blank');
             activityLink.textContent = link.title;
             listWeek.appendChild(activityLink);
+
+            if (index < week.links.length - 1) {
+                let separator = document.createTextNode(' | ');
+                listWeek.appendChild(separator);
+            }
         })
 
         list.appendChild(listWeek);
